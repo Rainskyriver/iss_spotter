948ac9147d2155929cc836647c5b6562f1311ac0
Lian Yelich <lianyelich14@gmail.com>
2020-01-17 02:29:51 +0000

    Added a promised version of iss.js called iss_promised.js and a index2.js to keep it modular

---
+++ index2.js
@@ -0,0 +1,12 @@
+// const { fetchMyIP } = require('./iss_promised');
+// const { fetchCoordsByIP } = require('./iss_promised');
+// const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    console.log(passTimes);
+  })
+  .catch((error) => {
+    console.log("Something messed up", error.message);
+  }); 
